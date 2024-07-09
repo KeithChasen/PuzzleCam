4cfddfa209bd7f364d6f62173247153ee628c78b
keithchasen <keithchasen89@gmail.com>
2024-07-09 21:16:49 +0200

    set up canvas / camera / video

---
+++ script.js
@@ -0,0 +1,31 @@
+let VIDEO = null;
+let CANVAS = null;
+let CONTEXT = null;
+
+function main() {
+    CANVAS = document.getElementById('myCanvas');
+    CONTEXT = CANVAS.getContext('2d');
+    CANVAS.width = window.innerWidth;
+    CANVAS.height = window.innerHeight;
+
+    let promise = navigator.mediaDevices.getUserMedia({
+        video: true
+    });
+
+    promise
+    .then(signal => {
+        VIDEO=document.createElement('video');
+        VIDEO.srcObject = signal;
+        VIDEO.play();
+
+        VIDEO.onloadeddata = () => {
+            updateCanvas();
+        }
+    })
+    .catch(err => alert(`Camera error: ${err}`))
+}
+
+function updateCanvas() {
+    CONTEXT.drawImage(VIDEO, 0,0);
+    window.requestAnimationFrame(updateCanvas);
+}
